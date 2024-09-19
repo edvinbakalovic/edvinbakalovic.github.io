287ec755318b3c5bf02eba97571d01bbcd56a074
edvinbakalovic <edvin.bakalovic04@gmail.com>
2024-09-19 20:12:10 +0200

    Implementation of Express server and establishment of connection between backend and frontend

--- react/src/App.js
+++ react/src/App.js
@@ -1,17 +1,53 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [name,setName] = useState('');
+  const [datetime,setDatetime] = useState('');
+  const [description,setDescription] = useState('');
+  function addNewTransaction(ev){
+      ev.preventDefault(); // kontra od defaultnog
+      const url=process.env.REACT_APP_API_URL+'/transaction';
+      console.log(url);
+      fetch(url,{
+        method:'POST',
+        headers:{'Content-Type:':'application/json'},
+        body: JSON.stringify({name,description,datetime})
+      }).then(response=>{
+        response.json().then(json=>{
+          console.log('result',json);
+        });
+      });
+  }
+
+
   return (
     <main>
       <h1>$400<span>.00</span></h1>
-      <form >
+      <form onSubmit={addNewTransaction}>
         <div className="basic">
-        <input type="text" placeholder={'+200 new samsung tv'} />
-        <input type="datetime-local" />
+        <input type="text" 
+        value={name} 
+        onChange={el=>{
+          setName(el.target.value);
+          //console.log(name);
+        }}
+        placeholder={'+200 new samsung tv'} />
+        <input type="datetime-local" 
+        value={datetime}
+        onChange={el=>{
+          setDatetime(el.target.value);
+          //console.log(datetime);
+        }} />
         </div>
 
         <div className="description">
-          <input type="text" />
+          <input type="text" 
+          value={description}
+          onChange={el=>{
+            setDescription(el.target.value);
+            //console.log(description);
+          }} />
         </div>
 
         <button type="submit">Add new Transaction</button>
@@ -60,3 +96,5 @@ function App() {
 }
 
 export default App;
+
+
